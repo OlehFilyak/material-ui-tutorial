137327b28b36d9d967ee160656a331fb24f044f8
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-08-26 19:42:05 +0300

    added buttons component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,31 +1,44 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Container from '@material-ui/core/Container';
 
 export default function Create() {
   return (
-    <div>
+    <Container size="sm">
       <Typography
         variant="h6"
-        component="h2"
         color="textSecondary"
-        gutterBottom="true"
-      >
-        Create a new note
-      </Typography>
-
-      {/* <Typography
-        variant="h1" 
-        color="primary"
-        align="center"
+        component="h2"
+        gutterBottom
       >
         Create a New Note
       </Typography>
-      <Typography
-        noWrap
+
+      <Button
+        onClick={() => console.log('you clicked me')}
+        type="submit"
         color="secondary"
+        variant="contained"
       >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque molestiae magnam voluptate reiciendis fuga totam facere officia, animi sint laboriosam dolorum voluptas cupiditate explicabo dolore veritatis recusandae rem inventore sunt.
-      </Typography> */}
-    </div>
+        Submit
+      </Button>
+
+      <br />
+
+      <Button type="submit" color="secondary" variant="outlined">
+        Submit
+      </Button>
+      <Button type="submit" color="default">
+        Submit
+      </Button>
+
+      <ButtonGroup color="primary" variant="contained" disableElevation>
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+    </Container>
   );
 }
