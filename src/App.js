a5495897040004bc9ce2e05198431a389a8ea0c7
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-08-26 18:27:07 +0300

    add topography component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Notes from './pages/Notes'
+import Create from './pages/Create'
 
 function App() {
-  return <div className="App">Main page</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Notes />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
